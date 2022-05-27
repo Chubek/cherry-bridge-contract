dd492aa92ba71786fb457d97f7e2f4231ebeaf5a
Chubak <chubakbidpaa@gmail.com>
2022-05-27 23:12:29 +0430

    submitted for review

--- lib.rs
+++ lib.rs
@@ -1,5 +1,6 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+
 use ink_lang as ink;
 
 mod utils {
@@ -11,12 +12,21 @@ mod utils {
     use scale::{Decode, Encode};
     use scale_info::TypeInfo;
 
-    fn hex_decode(s: &[u8], buffer: &mut [u8]) {
-        hex::decode_to_slice(s, buffer).unwrap();
+    fn hex_decode(s: &[u8]) -> [u8; 32] {
+        let res = hex::decode(s).unwrap();
+
+	    let  mut buffer = [0u8; 32];
+	
+	    for i in (0..res.len()).rev() {
+
+		    buffer[32 - (res.len() - i)] = res[i];
+	    }
+        
+        buffer
     }
 
-    fn hex_encode(h: &[u8], buffer: &mut [u8]) {
-        hex::encode_to_slice(h, buffer).unwrap()
+    fn hex_encode(h: &[u8]) -> String {
+        hex::encode(h)
     }
     #[derive(
         StorageLayout,
@@ -58,13 +68,9 @@ mod utils {
         pub fn to_string_digest(&self) -> String {
             let digest = self.digest;
             let digest_slice = digest.as_slice();
+      
 
-            let mut buffer: [u8; 32] = Default::default();
-            let buffer_slice = buffer.as_mut_slice();
-
-            hex_encode(digest_slice, buffer_slice);
-
-            core::str::from_utf8(buffer_slice).unwrap().to_string()
+            hex_encode(digest_slice)
         }
 
         pub fn to_string(&self) -> String {
@@ -107,10 +113,8 @@ mod utils {
     pub struct U256([u8; 32]);
 
     impl U256 {
-        pub fn from(b: &[u8]) -> Self {
-            let mut buffer: [u8; 32] = Default::default();
-
-            hex_decode(b, buffer.as_mut_slice());
+        pub fn from_hex(b: &[u8]) -> Self {
+            let buffer = hex_decode(b);            
 
             U256(buffer)
         }
@@ -121,13 +125,187 @@ mod utils {
             b
         }
 
-        pub fn to_string(&self) -> String {
-            let arr = self.get();
+        pub fn from_decimal(s: String) -> Self {
+            fn add(s1: String, s2: String) -> String {
+                let (mut result, mut stmp)  = ("".to_string(), "".to_string());
+                let  mut mark = 0;
+                
+                fn string_to_array_reversed(s: String) -> [u32; 256] {
+                    let mut chars = s.chars();
+
+                    let mut ret = [03u32; 256];
+
+                    for i in 0..chars.clone().count() {
+                        ret[i] = chars.next_back().unwrap().to_digit(10).unwrap(); 
+                    }
+
+                    ret
+                }
+
+                let smaller = if s1.len() > s2.len() { s2.clone() } else { s1.clone() };
+                let larger = if s1.len() > s2.len() { s1.clone() } else { s2.clone() };
+
+                let a1 = string_to_array_reversed(larger.clone());
+                let a2 = string_to_array_reversed(smaller.clone());
+
+                for i in 0..larger.len() {
+                    let ctmp = a1[i] + (if i < smaller.len() { a2[i] } else { 0 }) + mark;
+
+                    match ctmp {
+                        0 => {
+                            stmp = "0".to_string();
+                            mark = 0;
+                        },
+                        1 => {
+                            stmp = "1".to_string();
+                            mark = 0;
+                        },
+                        2 => {
+                            stmp = "0".to_string();
+                            mark = 1;
+                        },
+                        3 => {
+                            stmp = "1".to_string();
+                            mark = 1;
+                        },
+                        _ => (),
+                    }
+
+                    let mut r = "".to_string();
+                    r.push_str(stmp.as_str());
+                    r.push_str(result.as_str());
+
+                    result = r;
+                }
+
+                if mark > 0 {
+                    let mut r = "1".to_string();
+                    r.push_str(result.as_str());
+
+                    result = r
+                }
+
+                result
+
+            }         
+            
+            fn digit_to_bin(c: char) -> &'static str {
+            	match c {
+            		'0' => "0",
+            		'1' => "1",
+            		'2' => "10",
+            		'3' => "11",
+            		'4' => "100",
+            		'5' => "101",
+            		'6' => "110",
+            		'7' => "111",
+            		'8' => "1000",
+            		'9' => "1001",
+            		_  => "",
+            	
+            	}
+            
+            }
+            
+
+            let mut result = "".to_string();
+            let mut s_chars = s.chars();
+
+            for _ in 0..s.len() {
+                let mut r1 = result.clone();
+                let mut r2 = result.clone();
+
+                r1.push_str("0");
+                r2.push_str("000");
+
+                result = add(r1, r2);
+                result = add(result,  digit_to_bin(s_chars.next().unwrap()).to_string());
+                
+            }
+            
+
+            
+            let diff = 256 - result.len();
+            
+            let mut r = "".to_string();
+            
+            for _ in 0..diff {
+            	r.push_str("0");            
+            }
+            
+            r.push_str(result.as_str());
+            
+            result = r;
+            
+
+            
             let mut buffer = [0u8; 32];
+            
+	
 
-            hex_encode(arr as &[u8], &mut buffer as &mut [u8]);
+           for i in (0..256).step_by(8) {
+            	let sub_string = &result[i..i + 8];
 
-            core::str::from_utf8(&buffer).unwrap().to_string()
+            	let byte = u8::from_str_radix(sub_string, 2).unwrap();
+            		
+            	buffer[i / 8] = byte;            		
+            		
+           }            
+		    
+		 
+            Self(buffer)
+
+
+        }
+
+        pub fn to_decimal(&self) -> String {
+            let b = self.get();
+
+            let mut digits = [0u16; 78];
+            let mut length = 1;
+            
+            for j in 0..32 {
+                
+                let (mut i, mut carry) = (0usize, b[j] as u16);
+                
+                while i < length || carry !=0 {
+                    let mut value = digits[i] * 256 + carry;
+                    
+                    carry = value / 10;
+                    value = value % 10;
+                    
+                    digits[i] = value;	
+                    
+                    
+                    i += 1;	
+                }
+                
+                if i > length {
+                    length = i;
+                    
+                }
+            
+            }
+            
+            let mut number = "".to_string();
+            println!("l {length}");
+            for k in (0..length).rev() {
+                number.push_str(digits[k].to_string().as_str());
+            
+            }
+            
+            
+            number	
+        
+        
+        }          
+        
+        
+
+        pub fn to_hex(&self) -> String {
+            let arr = self.get();
+
+            hex_encode(arr as &[u8])
         }
 
         pub fn a_greater_than_b(a: &Self, b: &Self) -> bool {
@@ -173,7 +351,7 @@ mod utils {
                 }
             }
 
-            Self::from(&addition_arr as &[u8])
+            Self(addition_arr)
         }
 
         pub fn subtract_b_from_a(a: &Self, b: &Self) -> Self {
@@ -201,21 +379,31 @@ mod utils {
                 }
             }
 
-            Self::from(&addition_arr as &[u8])
+            Self(addition_arr)
         }
 
         pub fn new_zero() -> Self {
             let b = [0u8; 32];
 
-            Self::from(&b)
+            Self(b)
+        }
+
+        pub fn new_ff() -> Self {
+            let b = [0xffu8; 32];
+
+            Self(b)
         }
     }
 
     impl From<String> for U256 {
         fn from(s: String) -> Self {
-            let b = s.as_bytes();
-
-            Self::from(b)
+            match s.chars().next().unwrap() {
+                '0' => {
+                    let b = s.as_bytes();
+                    Self::from_hex(b)
+                },
+                _ => Self::from_decimal(s),
+            }   
         }
     }
 }
@@ -287,7 +475,8 @@ mod bridge_cherry_contract {
         owner: String,
         #[ink(topic)]
         spender: String,
-        value: String,
+        value_decimal: String,
+        value_hex: String,
     }
 
     #[ink(event)]
@@ -296,7 +485,8 @@ mod bridge_cherry_contract {
         from: Option<String>,
         #[ink(topic)]
         to: Option<String>,
-        value: String,
+        value_decimal: String,
+        value_hex: String,
     }
 
     #[derive(scale::Encode, scale::Decode, scale_info::TypeInfo)]
@@ -327,7 +517,7 @@ mod bridge_cherry_contract {
             Self::env().emit_event(Initiate {
                 initiated: true,
                 by: addr_multi.to_string(),
-                initial_balance: initial_supply.to_string(),
+                initial_balance: initial_supply.to_decimal(),
             });
         }
 
@@ -373,7 +563,8 @@ mod bridge_cherry_contract {
             Self::env().emit_event(Transfer {
                 from: Some(from.to_string()),
                 to: Some(to.to_string()),
-                value: value.to_string(),
+                value_decimal: value.to_decimal(),
+                value_hex: value.to_hex(),
             });
 
             Ok(())
@@ -391,12 +582,13 @@ mod bridge_cherry_contract {
             self.env().emit_event(Approval {
                 owner: owner.to_string(),
                 spender: spender.to_string(),
-                value: value.to_string(),
+                value_decimal: value.to_decimal(),
+                value_hex: value.to_hex(),
             });
         }
 
         #[ink(message)]
-        pub fn bridge_in(
+        pub fn bridge(
             &mut self,
             token_address: MultiChainAddrHash,
             token_amount: U256,
@@ -416,7 +608,7 @@ mod bridge_cherry_contract {
         }
 
         #[ink(message)]
-        pub fn bridge_in_string(
+        pub fn bridge_string(
             &mut self,
             token_address_str: String,
             token_amount_str: String,
@@ -429,7 +621,7 @@ mod bridge_cherry_contract {
             let recipient = recipient_str.clone().into();
             let from_chain = from_chain_str.clone().into();
 
-            let res = self.bridge_in(token_address, token_amount, recipient, from_chain);
+            let res = self.bridge(token_address, token_amount, recipient, from_chain);
 
             if emit_simple {
                 Self::env().emit_event(BridgeInSimple {
@@ -444,16 +636,19 @@ mod bridge_cherry_contract {
         }
 
         #[ink(message)]
-        pub fn bridge_out(
+        pub fn bridge_in(
             &mut self,
-            token_address: MultiChainAddrHash,
             token_amount: U256,
+            recipient: MultiChainAddrHash,
             from_chain: U256,
         ) -> Result<(), BridgeContractError> {
             let caller = self.env().caller();
-            let recipient: MultiChainAddrHash = (caller.as_ref() as &[u8]).into();
+            let token_address: MultiChainAddrHash = (caller.as_ref() as &[u8]).into();
 
-            let res = self.transfer_from_to(&token_address, &recipient, token_amount);
+            let res = self.transfer_from_to(
+                &token_address,
+                 &recipient, 
+                 token_amount);
 
             Self::env().emit_event(BridgeInComplex {
                 token_address,
@@ -466,28 +661,88 @@ mod bridge_cherry_contract {
         }
 
         #[ink(message)]
+        pub fn bridge_in_string(
+            &mut self,
+            token_amount_str: String,
+            recipient_str: String,
+            from_chain_str: String,
+            emit_simple: bool,
+        ) -> Result<(), BridgeContractError> {
+            let token_amount = token_amount_str.clone().into();
+            let recipient: MultiChainAddrHash = recipient_str.clone().into();
+            let from_chain = from_chain_str.clone().into();
+
+            let res = self.bridge_in(
+                token_amount, 
+                recipient,
+                from_chain);
+
+            let caller = self.env().caller();
+            let token_address: MultiChainAddrHash = (caller.as_ref() as &[u8]).into();
+
+            if emit_simple {
+                Self::env().emit_event(BridgeInSimple {
+                    token_address: token_address.to_string(),
+                    token_amount: token_amount_str,
+                    recipient: recipient_str,
+                    from_chain: Some(from_chain_str),
+                });
+            }
+
+            res
+        }
+
+        #[ink(message)]
+        pub fn bridge_out(
+            &mut self,
+            token_address: MultiChainAddrHash,
+            token_amount: U256,
+            target_chain: U256,
+        ) -> Result<(), BridgeContractError> {
+            let caller = self.env().caller();
+            let recipient: MultiChainAddrHash = (caller.as_ref() as &[u8]).into();
+
+            let res = self.transfer_from_to(
+                &token_address,
+                 &recipient, 
+                 token_amount);
+
+            Self::env().emit_event(BridgeOutComplex {
+                token_address,
+                token_amount,
+                recipient,
+                target_chain: Some(target_chain),
+            });
+
+            res
+        }
+
+        #[ink(message)]
         pub fn bridge_out_string(
             &mut self,
             token_address_str: String,
             token_amount_str: String,
-            from_chain_str: String,
+            target_chain_str: String,
             emit_simple: bool,
         ) -> Result<(), BridgeContractError> {
             let token_address = token_address_str.clone().into();
             let token_amount = token_amount_str.clone().into();
-            let from_chain = from_chain_str.clone().into();
+            let target_chain = target_chain_str.clone().into();
 
-            let res = self.bridge_out(token_address, token_amount, from_chain);
+            let res = self.bridge_out(
+                token_address, 
+                token_amount, 
+                target_chain);
 
             let caller = self.env().caller();
             let recipient: MultiChainAddrHash = (caller.as_ref() as &[u8]).into();
 
             if emit_simple {
-                Self::env().emit_event(BridgeInSimple {
+                Self::env().emit_event(BridgeOutSimple {
                     token_address: token_address_str,
                     token_amount: token_amount_str,
                     recipient: recipient.to_string(),
-                    from_chain: Some(from_chain_str),
+                    target_chain: Some(target_chain_str),
                 });
             }
 
